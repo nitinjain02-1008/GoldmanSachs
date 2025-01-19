73186424ec846ba7023c238b530086d1a0f150c2
nitinjain02.1008@gmail.com <nitinjain02.1008@gmail.com>
2025-01-19 21:07:58 +0530

    Create DicountPrice.java
    Problem statement

---
+++ GS/src/main/java/com/com/test/Altimetrik/DicountPrice.java
@@ -0,0 +1,52 @@
+package com.test.Altimetrik;
+
+import java.util.*;
+
+public class DicountPrice {
+
+	/*
+	 * 
+	 	An e-commerce company is currently celebrating ten years in business. 
+	 	They are having a sale to honor their privileged members, those who have been using their services for the past five years. 
+	 	They receive the best discounts indicated by any discount tags attached to the product. 
+	 	Determine the minimum cost to purchase all products listed. 
+	 	As each potential price is calculated, round it to the nearest integer before adding it to the total. 
+	 	Return the cost to purchase all items as an integer. 
+	 	
+	 	There are three types of discount tags: 
+		• Type O: discounted price, the item is sold for a given price. 
+		• Type 1: percentage discount, the customer is given a fixed percentage discount from the retail price. 
+		• Type 2: fixed discount, the customer is given a fixed amount off from the retail price. 
+		Example 
+			products = [['10', 'do', 'd1'], ['15', 'EMPTY', 'EMPTY'], ['20', 'd1', 'EMPTY"]] 
+			discounts = [['d0','1','27'], ['d1', '2', '5']] 
+		Output --> 35
+	
+	Explanation:
+
+		Product 1: Minimum price is 5 (after applying discount d1).
+		Product 2: No discounts, so the price is 15.
+		Product 3: Minimum price is 15 (after applying discount d1).
+		Total cost = 
+		5 + 15 + 15 = 35
+		
+		The products array elements are in the form ['price', 'tag 1', 'tag 2',. , 'tag m-1'). There may be zero or more discount codes associated with a product.
+		Discount tags in the products array may be 'EMPTY' which is the same as a null value. 
+		The discounts array elements are in the form ['tag, 'type', 'amount'].
+
+*/
+	
+	public static int findLowestPrice(List<List<String>> products, List<List<String>> discounts) {
+        // WRITE YOUR BRILLIANT CODE HERE
+        
+		return 0;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+}
